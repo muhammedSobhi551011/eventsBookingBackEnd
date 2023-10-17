80d009f1a8e819316f58b52e49d1098d69d9799c
Muhammed Sobhi <mohamed.sophy89847@outlook.com>
2023-10-17 19:21:03 +0300

    first commit

---
+++ server.ts
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotEnv from "dotenv";
+
+// start express application
+const app: express.Application = express();
+
+// configure app to receive form data
+app.use(express.json());
+
+// cors
+app.use(cors());
+
+// configure env file
+dotEnv.config({ path: "./.env" });
+
+// conntect to MONGO_DB
+const mongoLocal: string | undefined = process.env.MONGO_DB_LOCAL;
+if (mongoLocal) {
+  mongoose
+    .connect(mongoLocal)
+    .then(() => {
+      console.log("Connected to MongoDB successfully.....");
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
+
+//---------------------------------
+// ROUTERS
+// test home router with GET request
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send("Welcome to social express app.");
+});
+//---------------------------------
+
+// define hostname and port
+const hostname: string | undefined = process.env.HOST_NAME;
+const port: string | undefined = process.env.PORT;
+
+// listen to express application
+if (hostname && port) {
+  app.listen(Number(port), hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}`);
+  });
+} else {
+  console.log("Check hostname and port!");
+}
